30ee440dcaadb34cb34898bf50898f1f332692dd
Shaedow2000 <amsal8795@gmail.com>
2025-12-10 15:24:38 +0100

    Create project stucture.

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print( '\n--> Exiting...' )
+        sys.exit( 1 )
